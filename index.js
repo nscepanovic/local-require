cc00b0bf780e2df891c616b92fc55b372d4078a8
Nemanja <nemanja@examiz.com>
2022-07-06 23:10:22 +0200

    path.reslove

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const path = require('path')
 
 module.exports = (filePath) => {
   return require(
-    process.env.NODE_ENV === 'local' ?
-      path.resolve(filePath) :
-      `./${filePath.split('/').slice(-1)[0]}`)
+    path.resolve(
+      process.env.NODE_ENV === 'local' ?
+      filePath : `./${filePath.split('/').slice(-1)[0]}`)
+  )
 }
