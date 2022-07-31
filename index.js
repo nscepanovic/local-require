20e1d7ca3f418fe4d09ed04c8649583bf33bf393
Nemanja <nemanja@examiz.com>
2022-07-31 16:42:45 +0200

    fix: typo

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports = (filePath) => {
   return require(
     path.resolve(
       process.env.NODE_ENV === 'local' ?
-      filePath : `./${value.split('/').filter(i => i !== '..').join('/')}`)
+      filePath : `./${filePath.split('/').filter(i => i !== '..').join('/')}`)
   )
 }
